5f3dea748ddbeaa41fdac8dab0ac70bcc366af5a
”Anuron <anuronmaitro@gmail.com>
2024-12-12 18:35:16 +0600

    Added Fun.py Added Some Dummy Comments from Hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
 print("Hello, World!")
+#git init
+#git add
+#git commit -m "first commit"
+#git status
+#git log
+#git add -A =meaning all files added
+#git add . = add all files in the current directory
+# git add -U= add all updated files
